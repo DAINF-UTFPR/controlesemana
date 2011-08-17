1c0038991d45522930d6ad5896adec02ef594b75
Luiz Ribeiro <luizribeiro@gmail.com>
2011-08-17 20:31:28 -0300

    Commit inicial, mockup da GUI

---
+++ src/controlesemana/Main.java
@@ -0,0 +1,16 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <luizribeiro@gmail.com> wrote this file. As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you
+ * think this stuff is worth it, you can buy me a beer in return. Luiz Ribeiro
+ * ----------------------------------------------------------------------------
+ */
+package controlesemana;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new GUIWindow().setVisible(true);
+    }
+}
