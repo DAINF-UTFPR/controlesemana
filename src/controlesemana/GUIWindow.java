294ad8ea10abe0ced1570d2b480cb7e8d57aad6f
Luiz Ribeiro <luizribeiro@gmail.com>
2011-08-17 22:12:35 -0300

    Licença no GUIWindow.java

--- src/controlesemana/GUIWindow.java
+++ src/controlesemana/GUIWindow.java
@@ -1,21 +1,15 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * GUIWindow.java
- *
- * Created on Aug 17, 2011, 7:49:17 PM
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <luizribeiro@gmail.com> wrote this file. As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you
+ * think this stuff is worth it, you can buy me a beer in return. Luiz Ribeiro
+ * ----------------------------------------------------------------------------
  */
 package controlesemana;
 
 import java.awt.event.KeyEvent;
 
-/**
- *
- * @author luiz
- */
 public class GUIWindow extends javax.swing.JFrame {
     
     protected boolean stateEntrada;
